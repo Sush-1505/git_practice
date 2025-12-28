d6585536e7dbddfaba7327f548acfc4e957ef7a4
Swati-Deshvena <swatiraskatla19@gmail.com>
2025-12-28 18:00:08 +0530

    problem

--- src/test/java/arrayPrograms/Gitpractice.java
+++ src/test/java/arrayPrograms/Gitpractice.java
@@ -1,11 +1,10 @@
 package arrayPrograms;
 
-public class Newprogram {
+public class Gitpractice {
 
 	public static void main(String[] args) {
-		
-		System.out.println("abc");
 
+		System.out.println("abc");
 	}
 
 }
