b15669f843f47ee5878e898143f6c7c997384a4b
Swati-Deshvena <swatiraskatla19@gmail.com>
2025-12-28 17:19:36 +0530

    addition program

--- src/test/java/arrayPrograms/AdditionOfWholeArray.java
+++ src/test/java/arrayPrograms/AdditionOfWholeArray.java
@@ -6,19 +6,16 @@ public class AdditionOfWholeArray {
 
 		int x[] = { 2, 2, 4, 6, 8, 2, 5 };
 
-<<<<<<< HEAD
 
 		int sum = 1;
-=======
-		int sum = 0;
->>>>>>> 7521b21966a906d64750e413241a8f220b05d461
+		int sum1 = 0;
 
 		for (int i = 0; i < x.length; i++) {
        
-			sum=sum*x[i];
+			sum1=sum1*x[i];
 		}
 
-		System.out.println("addition Of Whole Array= " + sum);
+		System.out.println("addition Of Whole Array= " + sum1);
 	}
 
 }
